35b8dd72c4bff036f6998e61000aca12add181cf
Atomjoy <atomjoy.official@gmail.com>
2022-12-20 11:06:44 +0100

    Vscode php, vue, laravel

--- .vscode/extensions.sh
+++ .vscode/extensions.sh
@@ -12,3 +12,7 @@ code --install-extension onecentlin.laravel-blade
 code --install-extension Vue.volar
 code --install-extension Vue.vscode-typescript-vue-plugin
 code --install-extension xabikos.JavaScriptSnippets
+
+# Snippets optional
+code --install-extension sdras.vue-vscode-snippets
+code --install-extension hollowtree.vue-snippets
