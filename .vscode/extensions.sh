75bc7bdf33ee28a668dda74b10392257e847eb2f
atomjoy <atomjoy.official@gmail.com>
2023-06-14 10:56:46 +0200

    Update extensions.sh

--- .vscode/extensions.sh
+++ .vscode/extensions.sh
@@ -9,10 +9,10 @@ code --install-extension PKief.material-icon-theme
 
 # Php, vue, js
 code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension DotJoshJohnson.xml
 code --install-extension esbenp.prettier-vscode
 code --install-extension onecentlin.laravel-blade
 code --install-extension Vue.volar
 code --install-extension Vue.vscode-typescript-vue-plugin
 code --install-extension xabikos.JavaScriptSnippets
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension dotjoshjohnson.xml
