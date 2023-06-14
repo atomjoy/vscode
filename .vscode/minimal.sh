2f9f8f376c5f52f003608b0406d3c0d1e7609ac2
atomjoy <atomjoy.official@gmail.com>
2023-06-14 10:54:22 +0200

    Create minimal.sh
    Vscode extensions 2023

---
+++ .vscode/minimal.sh
@@ -0,0 +1,12 @@
+# Install extensions in terminal
+
+code --install-extension BeardedBear.beardedtheme
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension DotJoshJohnson.xml
+code --install-extension esbenp.prettier-vscode
+code --install-extension onecentlin.laravel-blade
+code --install-extension PKief.material-icon-theme
+code --install-extension Vue.volar
+code --install-extension Vue.vscode-typescript-vue-plugin
+code --install-extension xabikos.JavaScriptSnippets
