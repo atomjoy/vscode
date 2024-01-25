27f432b88aa3260b0081f5ac9bc28a097ec37e96
Atomjoy <atomjoy.official@gmail.com>
2024-01-25 16:17:47 +0100

    Update

--- .vscode/extensions.sh
+++ .vscode/extensions.sh
@@ -8,11 +8,16 @@ code --install-extension GitHub.github-vscode-theme
 code --install-extension PKief.material-icon-theme
 
 # Php, vue, js
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
 code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension cnmorocho.min-gruvbox-theme
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension DotJoshJohnson.xml
 code --install-extension esbenp.prettier-vscode
 code --install-extension onecentlin.laravel-blade
+code --install-extension shufo.vscode-blade-formatter
+code --install-extension wheredoesyourmindgo.gruvbox-concoctis
+code --install-extension xabikos.JavaScriptSnippets
 code --install-extension Vue.volar
 code --install-extension Vue.vscode-typescript-vue-plugin
-code --install-extension xabikos.JavaScriptSnippets
